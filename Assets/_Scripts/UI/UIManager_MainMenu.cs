ece627f3ecbf0e2b2d8b1b34c53d6e316e94a03e
victor-Lopez25 <victorlopezcortes25@gmail.com>
2024-02-20 18:53:16 +0100

    push prueba

---
+++ Assets/_Scripts/UI/UIManager_MainMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager
+{
+    private MenuManager() { }
+    public static void SoundEffectsToggleOnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("SoundEffects", value ? 1 : 0);
+        //AudioManager.Instance.ToggleEffects(value);
+    }
+
+    public static void MusicToggleOnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("Music", value ? 1 : 0);
+        //AudioManager.Instance.ToggleMusic(value);
+    }
+}
+
+public class UIManager_MainMenu : MonoBehaviour
+{
+    [SerializeField] protected Button playButton;
+    [SerializeField] protected Button exitButton;
+
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private Toggle sfxToggle;
+
+    protected void Awake()
+    {
+        playButton.onClick.AddListener(PlayButtonOnClick);
+        exitButton.onClick.AddListener(ExitButtonOnClick);
+
+        sfxToggle.onValueChanged.AddListener(MenuManager.SoundEffectsToggleOnValueChanged);
+        musicToggle.onValueChanged.AddListener(MenuManager.MusicToggleOnValueChanged);
+
+        sfxToggle.isOn = PlayerPrefs.GetInt("SoundEffects", 0) == 1;
+        musicToggle.isOn = PlayerPrefs.GetInt("Music", 0) == 1;
+
+        //AudioManager.Instance.ToggleEffects(soundEffectsToggle.isOn);
+        //AudioManager.Instance.ToggleMusic(musicToggle.isOn);
+    }
+
+    private void ExitButtonOnClick()
+    {
+        Application.Quit();
+    }
+
+    private void PlayButtonOnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
