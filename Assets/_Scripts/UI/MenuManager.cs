a8d585af35e8557c5cddab1f1bf0a274d6d538b2
victor-Lopez25 <victorlopezcortes25@gmail.com>
2024-02-20 20:28:34 +0100

    menú

---
+++ Assets/_Scripts/UI/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MenuManager
+{
+    private MenuManager() { }
+    public static void SoundEffectsToggleOnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("SoundEffects", value ? 1 : 0);
+        //AudioManager.Instance.ToggleEffects(value);
+    }
+
+    public static void MusicToggleOnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("Music", value ? 1 : 0);
+        //AudioManager.Instance.ToggleMusic(value);
+    }
+}
--- Assets/_Scripts/UI/UIManager_MainMenu.cs
+++ Assets/_Scripts/UI/UIManager_MainMenu.cs
@@ -3,22 +3,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MenuManager
-{
-    private MenuManager() { }
-    public static void SoundEffectsToggleOnValueChanged(bool value)
-    {
-        PlayerPrefs.SetInt("SoundEffects", value ? 1 : 0);
-        //AudioManager.Instance.ToggleEffects(value);
-    }
-
-    public static void MusicToggleOnValueChanged(bool value)
-    {
-        PlayerPrefs.SetInt("Music", value ? 1 : 0);
-        //AudioManager.Instance.ToggleMusic(value);
-    }
-}
-
 public class UIManager_MainMenu : MonoBehaviour
 {
     [SerializeField] protected Button playButton;
