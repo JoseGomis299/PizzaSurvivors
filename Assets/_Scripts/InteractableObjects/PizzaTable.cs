ffd968ec08e5472af7c5c334f5d177ba0895a408
JoseGomis299 <joseg03a@gmail.com>
2024-02-22 17:52:39 +0100

    mapa provisional

--- Assets/_Scripts/InteractableObjects/PizzaTable.cs
+++ Assets/_Scripts/InteractableObjects/PizzaTable.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PizzaTable : MonoBehaviour, IInteractable
@@ -6,11 +7,36 @@ public class PizzaTable : MonoBehaviour, IInteractable
     private Pizza _pizza;
     
     private SpriteRenderer _spriteRenderer;
-    
+    private Color _defaultColor;
+
+
+    private void Awake()
+    {
+        SpawningSystem.OnRoundStart += OnRoundStart;
+        SpawningSystem.OnRoundEnd += OnRoundEnd;
+    }
+
     private void Start()
     {
         _pizza = pizzaUI.GetComponent<Pizza>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultColor = _spriteRenderer.color;
+    }
+
+    private void OnDestroy()
+    {
+        SpawningSystem.OnRoundStart -= OnRoundStart;
+        SpawningSystem.OnRoundEnd -= OnRoundEnd;
+    }
+
+    private void OnRoundStart(int round)
+    {
+        gameObject.SetActive(false);
+    }
+    
+    private void OnRoundEnd(int round)
+    {
+        gameObject.SetActive(true);
     }
 
 
@@ -21,7 +47,7 @@ public class PizzaTable : MonoBehaviour, IInteractable
     
     public void OnInteractRangeExit()
     {
-        _spriteRenderer.color = Color.cyan;
+        _spriteRenderer.color = _defaultColor;
     }
     
     public void Interact()
